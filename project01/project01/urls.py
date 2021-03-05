e556098583f52bf7a73a749dad2e3071712c93aa
JuanVicenteVazquezG <dev@juanvicentevazquezg.es>
2021-03-05 11:31:59 +0100

    settings.py split into 3 pieces

--- project01/project01/urls.py
+++ project01/project01/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+def pruebaUrl(self):
+	print('====================== Probando el fichero Urls=================================')
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+   	path('prueba/', pruebaUrl),
 ]
