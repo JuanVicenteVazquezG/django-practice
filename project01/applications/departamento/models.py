c84fbe3077ae58f0c1dbcfdc9409cf012306b003
JuanVicenteVazquezG <dev@juanvicentevazquezg.es>
2021-03-07 22:25:30 +0100

    dbpostgres created

--- project01/applications/departamento/models.py
+++ project01/applications/departamento/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class Departamento(models.Model):
-    name = models.CharField('Nombre', max_length=50, editable=False)
+    name = models.CharField('Nombre', max_length=50, editable=True)
     short_name = models.CharField('Nombre Corto', max_length=20, unique=True)
     anulate = models.BooleanField('Anulado', default=False)
 
     def __str__(self):
-        return f" {str(pk)} {name} - {short_name}"
+        return f"{str(self.pk)} -  {self.name} - {self.short_name}"
--- project01/applications/persona/models.py
+++ project01/applications/persona/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+
+# FOREING KEYS
+
+from applications.departamento.models import Departamento
+
+
+class Empleado(models.Model):
+    """ Modelo para tabla empleado """
+
+    # choice
+    JOB_CHOICES = (
+        ('0', 'CONTADOR'),
+        ('1', 'ADMINISTRADOR'),
+        ('2', 'ECONOMISTA'),
+        ('3', 'OTRO')
+    )
+    first_name = models.CharField("Nombres", max_length=60)
+    last_name = models.CharField("Aoellidos", max_length=60)
+    job = models.CharField("Trabajo", max_length=50, choices=JOB_CHOICES)
+    departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
+    # imagen = models.ImageField(, upload_to=None, height_field=None, width_field=None)
+
+    def __str__(self):
+        return f"{str(self.pk)} - {self.first_name} - {self.last_name}"
