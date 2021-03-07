c6a1ef20582a05337ce2ee4fbc2b21afa0b3aa45
JuanVicenteVazquezG <dev@juanvicentevazquezg.es>
2021-03-07 13:15:55 +0100

    Generic Views sample finished

--- project01/applications/home/models.py
+++ project01/applications/home/models.py
@@ -5,7 +5,7 @@ from django.db import models
 
 class Prueba(models.Model):
     titulo = models.CharField(max_length=100)
-    subtititulo = models.CharField(max_length=50)
+    subtitulo = models.CharField(max_length=50)
     cantidad = models.IntegerField()
 
     def __str__(self):
--- project01/applications/home/views.py
+++ project01/applications/home/views.py
@@ -4,7 +4,8 @@ from django.shortcuts import render
 
 # Vistas generias
 
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, CreateView
+from .models import Prueba
 
 
 class PruebaView(TemplateView):
@@ -16,3 +17,15 @@ class PruebaListView(ListView):
     template_name = "home/lista.html"
     context_object_name = 'lista_numeros'
     queryset = ['1', '2', '3', '4']
+
+
+class ListarPrueba(ListView):
+    template_name = "home/lista_prueba.html"
+    model = Prueba
+    context_object_name = 'lista_prueba'
+
+
+class PruebaCreateView(CreateView):
+    model = Prueba
+    template_name = "home/add.html"
+    fields = ['titulo', 'subtitulo', 'cantidad']
