811f20a4de185d3aa6c242081385785f2aa5e2b8
JuanVicenteVazquezG <dev@juanvicentevazquezg.es>
2021-03-08 08:41:40 +0100

    Habilidades class added

--- project01/applications/persona/models.py
+++ project01/applications/persona/models.py
@@ -7,6 +7,17 @@ from django.db import models
 from applications.departamento.models import Departamento
 
 
+class Habilidades(models.Model):
+    habilidades = models.CharField('habilidad', max_length=50)
+
+    class Meta:
+        verbose_name = "habilidad"
+        verbose_name_plural = "habilidades Empleados"
+
+    def __str__(self):
+        return f"{str(self.id)} - {self.first_habilidad}"
+
+
 class Empleado(models.Model):
     """ Modelo para tabla empleado """
 
@@ -21,7 +32,15 @@ class Empleado(models.Model):
     last_name = models.CharField("Aoellidos", max_length=60)
     job = models.CharField("Trabajo", max_length=50, choices=JOB_CHOICES)
     departamento = models.ForeignKey(Departamento, on_delete=models.CASCADE)
-    # imagen = models.ImageField(, upload_to=None, height_field=None, width_field=None)
+    avatar = models.ImageField(upload_to='empleado', blank=True, null=True)
+    habilidades = models.ManyToManyField(Habilidades)
+
+    class Meta:
+        verbose_name = 'Mi Empleado'
+        verbose_name_plural = " Empleados de la empresa"
+        # ordenacion descendente por el primer nombre
+        ordering = ['-first_name', 'departamento']
+        unique_together = ('first_name', 'departamento')
 
     def __str__(self):
         return f"{str(self.pk)} - {self.first_name} - {self.last_name}"
