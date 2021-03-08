43927d88c6e8ef6b522ed7761f6fc7dd5ad252fc
JuanVicenteVazquezG <dev@juanvicentevazquezg.es>
2021-03-08 08:05:08 +0100

    class meta added

--- project01/applications/departamento/models.py
+++ project01/applications/departamento/models.py
@@ -8,5 +8,11 @@ class Departamento(models.Model):
     short_name = models.CharField('Nombre Corto', max_length=20, unique=True)
     anulate = models.BooleanField('Anulado', default=False)
 
+    class Meta:
+        verbose_name = "Mis departamentos"
+        verbose_name_plural = "Areas de la empresa"
+        ordering = ['name']
+        unique_together = ('name', 'short_name')
+
     def __str__(self):
         return f"{str(self.pk)} -  {self.name} - {self.short_name}"
